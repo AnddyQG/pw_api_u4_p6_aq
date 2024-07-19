71561d5572af004af8a1733ed645ff8f11b5dbdf
AnddyQG <anddysk819@gmail.com>
2024-07-18 23:02:07 -0500

    tarea 10

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+ <!--  <EstudianteMain /> -->
+   <EstudianteEliminar /> 
+  <!--<EstudianteGuardar />-->
+</template>
+
+<script>
+ //import EstudianteMain from "./pages/EstudianteMain.vue";
+ import EstudianteEliminar from "./pages/EstudianteEliminar.vue";
+//import EstudianteGuardar from "./pages/EstudianteGuardar.vue";
+
+export default {
+  name: "App",
+  components: {
+    // EstudianteMain,
+     EstudianteEliminar,
+    //EstudianteGuardar,
+  }
+};
+
+</script>
+
+<style>
+#app {
+  font-family: 'Times New Roman', Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
